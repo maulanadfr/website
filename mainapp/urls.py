dc3e133bc8cd635733db1217685a5176859c62cb
maulanadfr <maulanar552@gmail.com>
2021-08-10 23:34:37 +0700

    first commit

---
+++ mainapp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('count/<int:angka>/', views.count),
+    path('sapa/<str:nama>/', views.sapa),
+    path('shop/', views.shop),
+    path('shop/laptop/', views.shop_laptop),
+    path('shop/smartphone/', views.shop_smartphone),
+    path('shop/console/', views.shop_console),
+    path('firstpage/', views.first_page),
+    path('secondpage/', views.second_page),
+    path('example/', views.example),
+    path('profile/', views.profile),
+    path('second/', views.second_page),
+    path('', views.landing_page),
+]
---
+++ mainapp/views.py
@@ -0,0 +1,59 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def landing_page(request):
+    return HttpResponse("Hello, World! Nama Saya Ariq")
+
+
+def profile(request):
+    return HttpResponse("profile ku")
+
+
+def count(request, angka):
+    angka = angka+1
+    return HttpResponse(str(angka))
+
+
+def sapa(request, nama):
+    return HttpResponse("Halo "+nama)
+
+
+def example(request):
+    return render(request, 'example.html')
+
+
+def newpage(request):
+    return HttpResponse("new")
+
+
+def a(request):
+    return HttpResponse()
+
+
+def shop(request):
+    return render(request, 'shop.html')
+
+
+def shop_laptop(request):
+    return render(request, 'shop_laptop.html')
+
+
+def shop_console(request):
+    return render(request, 'shop_console.html')
+
+
+def shop_smartphone(request):
+    return render(request, 'shop_smartphone.html')
+
+
+def first_page(request):
+    return render(request, 'firstpage.html')
+
+
+def second_page(request):
+    return render(request, 'secondpage.html')
+
+# Create your views here.
